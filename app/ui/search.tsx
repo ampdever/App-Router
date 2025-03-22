e8e711727840ee105895396086031e176fd999f5
Aaron Magallanes <aaronmagallanes@Aarons-MacBook-Air.local>
2025-03-22 03:02:32 -0600

    fixed lint errors

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -10,8 +10,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
-    const handleSearch = useDebouncedCallback((term) => {
+  //function handleSearch(term: string) {
+    const handleSearch = useDebouncedCallback((term: string) => {
       console.log(`Searching... ${term}`);
       const params = new URLSearchParams(searchParams);
       params.set('page', '1');
@@ -22,7 +22,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       }
       replace(`${pathname}?${params.toString()}`);
     }, 300);
-  }
+  //}
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
